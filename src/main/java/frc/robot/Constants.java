edd683f9a9e0919ef6b1316d12578fc145b4de07
Paul Garrett <paul.garrett@laingsburg.k12.mi.us>
2021-03-09 12:58:21 -0500

    trajectory example

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -4,18 +4,22 @@
 
 package frc.robot;
 
+import edu.wpi.first.wpilibj.kinematics.DifferentialDriveKinematics;
+
 /**
- * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
- * constants. This class should not be used for any other purpose. All constants should be declared
- * globally (i.e. public static). Do not put anything functional in this class.
+ * The Constants class provides a convenient place for teams to hold robot-wide
+ * numerical or boolean constants. This class should not be used for any other
+ * purpose. All constants should be declared globally (i.e. public static). Do
+ * not put anything functional in this class.
  *
- * <p>It is advised to statically import this class (or one of its inner classes) wherever the
- * constants are needed, to reduce verbosity.
+ * <p>
+ * It is advised to statically import this class (or one of its inner classes)
+ * wherever the constants are needed, to reduce verbosity.
  */
 public final class Constants {
 
-	public static final int LEFT_SPINNY_BOI = 1;
-	public static final int Right_SPINNY_BOI = 0;
+	public static final int LEFT_MOTOR = 1;
+	public static final int RIGHT_MOTOR = 0;
 
 	public static final int DRIVE_CONTROLLER = 0; // Driver Controller ID
     public static final int DRIVE_LEFT_X_AXIS = 0;
@@ -23,5 +27,29 @@ public final class Constants {
     public static final int DRIVE_LEFT_TRIGGER = 2;
     public static final int DRIVE_RIGHT_TRIGGER = 3;
     public static final int DRIVE_RIGHT_X_AXIS = 4;
-	public static final int DRIVE_RIGHT_Y_AXIS = 5;
-}
+    public static final int DRIVE_RIGHT_Y_AXIS = 5;
+    
+    public static final double ENCODER_METERS = .04454;
+    public static final double ENCODER_METERS_PER_SECOND = .000742;
+
+
+//feedforward/feedbackgains
+    public static final double ksVolts = 0;
+    public static final double kvVoltSecondsPerMeter = 0;
+    public static final double kaVoltsSecondsSquaredPerMeter = 0;
+
+    public static final double kPDriveVel =0; 
+//Differentialdrivekinematics
+    public static final double kTrackWidthMeters = .66;
+    public static final DifferentialDriveKinematics kDriveKinematics =
+        new DifferentialDriveKinematics(kTrackWidthMeters);
+//maxtrajectoryvelocity/acceleration
+    public static final double kMaxMeterPerSecond = 2;
+    public static final double kMaxAccelerationMetersPerSecondSquared = 2;
+//ramseteparameters
+    public static final double kRamseteB = 0;
+    public static final double kRamseteZeta = 0;
+
+
+        
+    }
--- src/main/java/frc/robot/subsystems/DriveTrainSubsystem.java
+++ src/main/java/frc/robot/subsystems/DriveTrainSubsystem.java
@@ -4,24 +4,40 @@
 
 package frc.robot.subsystems;
 
-import edu.wpi.first.wpilibj.PWMVictorSPX;
+import com.revrobotics.CANEncoder;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMax.IdleMode;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj.kinematics.DifferentialDriveWheelSpeeds;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 public class DriveTrainSubsystem extends SubsystemBase {
   
   /** Creates a new DriveTrainSubsystem. */
-  public DriveTrainSubsystem() {}
-  PWMVictorSPX left = new PWMVictorSPX(Constants.LEFT_SPINNY_BOI);
-  PWMVictorSPX right = new PWMVictorSPX(Constants.Right_SPINNY_BOI);
+  public DriveTrainSubsystem() {
+    m_leftencoder.setPositionConversionFactor(Constants.ENCODER_METERS);
+    m_rightencoder.setPositionConversionFactor(Constants.ENCODER_METERS);
+    m_leftencoder.setVelocityConversionFactor(Constants.ENCODER_METERS_PER_SECOND);
+    m_rightencoder.setVelocityConversionFactor(Constants.ENCODER_METERS_PER_SECOND);
+  }
+  CANSparkMax m_leftmotor = new CANSparkMax(Constants.LEFT_MOTOR, MotorType.kBrushless);
+  CANSparkMax m_rightmotor= new CANSparkMax(Constants.RIGHT_MOTOR, MotorType.kBrushless);
 
-  DifferentialDrive differentialRocketLeagueDrive = new DifferentialDrive(left, right);
+  CANEncoder m_leftencoder = m_leftmotor.getEncoder();
+  CANEncoder m_rightencoder = m_rightmotor.getEncoder();
+  
+
+  DifferentialDrive differentialRocketLeagueDrive = new DifferentialDrive(m_leftmotor, m_rightmotor);
   public void RocketLeagueDrive(double speed, double turn, double stop){
       double turning = 0.0;
       double moving = 0.0;
       double driveSpeed = 0.75; // Max drivespeed
       double rotateSpeed = 0.55; // How fast the bot turns while moving forward/backward
       double swivel = 0.45;  //How fast the bot pivots in place
+
+      setBrake();
       if(Math.abs(stop) > 0.5){
       } else if(speed >= 0.10 || speed <= -0.10){
   
@@ -38,8 +54,26 @@ public class DriveTrainSubsystem extends SubsystemBase {
   
     }
 
+    public void setBrake(){
+      m_leftmotor.setIdleMode(IdleMode.kBrake);
+      m_rightmotor.setIdleMode(IdleMode.kBrake);
+      SmartDashboard.putString("B/C Mode", "Brake");
+
+    }
+
+    public void setCoast(){
+      m_leftmotor.setIdleMode(IdleMode.kCoast);
+      m_rightmotor.setIdleMode(IdleMode.kCoast);
+      SmartDashboard.putString("B/C Mode", "Coast");
+    }
+    public DifferentialDriveWheelSpeeds getwheelspeeds() {
+      return new DifferentialDriveWheelSpeeds(m_leftencoder.getVelocity(), m_rightencoder.getVelocity());
+    }
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
+
+    
+
   }
 }
